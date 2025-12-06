5783aa6176f0ea74b35e4e1dffc24e689d4a1963
никита жук <nikitka.zhuk2007@gmail.com>
2025-12-06 13:40:38 +0300

    Создал кнопки статистика, выбор файла, запись речи, кто вы.

---
+++ SpeakUP_1/Login.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SpeakUP_1
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public string ResultData { get; private set; }
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TB_Me_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TB_Me.Text = "";
+            this.ResultData = TB_Me.Text;
+
+        }
+
+        private void Save_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.ResultData = TB_Me.Text;
+            this.DialogResult = true;
+        }
+    }
+}
--- SpeakUP_1/MainWindow.xaml.cs
+++ SpeakUP_1/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace SpeakUP_1
 {
@@ -20,14 +22,75 @@ namespace SpeakUP_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _loadedAudio;
         public MainWindow()
         {
             InitializeComponent();
         }
         int I = 1;
+        private void LoginB_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Login login = new Login();
+            bool? dialogResult = login.ShowDialog();
+            if (dialogResult == true)
+            {
+                string TB_Me = login.ResultData;
+            }
+            else if (dialogResult == false)
+            {
+                MessageBox.Show("Вы не рассказали о себе!");
+            }
+            else
+            {
+                MessageBox.Show("Вы не рассказали о себе!");
+            }
+        }
+
+        private void STOP_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (I == 0)
+            {
+                STOP.IsEnabled = false;
+                STOP.Margin = new Thickness(1000, 1000, 0, 0);
+                I = 1;
+                REC.IsEnabled = true;
+                REC.Margin = new Thickness(11, 0, 0, 10);
+                for (int i = 0; i < 5; i++)
+                {
+
+                }
+            }
+        }
+
         private void REC_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            
+            if (I == 1)
+            {
+                REC.IsEnabled = false;
+                REC.Margin = new Thickness(1000, 1000, 0, 0);
+                I = 0;
+                STOP.IsEnabled = true;
+                STOP.Margin = new Thickness(11, 0, 0, 10);
+            }
+        }
+
+        private void File_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Аудиозаписи (*.mp3;*.wav)|*.mp3;*.wav|Все файлы (*.*)|*.*";
+            openFileDialog.Title = "Выберите аудиозапись";
+            bool? result = openFileDialog.ShowDialog();
+            string selectedFilePath = openFileDialog.FileName;
+            if (result == true)
+            {
+                MessageBox.Show($"Файл успешно выбран: {selectedFilePath}");
+                _loadedAudio = openFileDialog.FileName;
+            }
+            else
+            {
+                MessageBox.Show("Файл не был выбран.");
+            }
         }
     }
 }
+ 
